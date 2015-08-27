c3145bfa00a291acfb889289a0871c691a15b2e4
Ciro S. Costa <ciro9758@gmail.com>
2015-08-26 22:37:31 -0300

    adds cli help to main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "yairc/connection.h"
 
+static const char* CLI_HELP =
+    "Usage: yairc <url> <service>\n"
+    "\n"
+    "url:\tserver authority (url w/out scheme and path\n"
+    "serv:\tservice to map port to.\n"
+    "\n"
+    "Example:\n"
+    "\t./yairc irc.mozilla.org dirc\n";
+
 void communicate(FILE* fp, int sockfd)
 {
   char sendline[YI_MAXLINE] = { 0 };
@@ -16,17 +25,18 @@ void communicate(FILE* fp, int sockfd)
 
 int main(int argc, char* argv[])
 {
-  yi_connection_t* connection; 
+  yi_connection_t* connection;
 
-  ASSERT(argc == 2, "Usage: yairc <url>");
+  if (argc != 3) {
+    fprintf(stderr, "%s\n", CLI_HELP);
+    return EXIT_FAILURE;
+  }
 
-  connection = yi_tcp_connect(argv[1], "echo");
+  connection = yi_tcp_connect(argv[1], argv[2]);
   communicate(stdin, connection->sockfd);
 
   yi_close(connection->sockfd);
   yi_delete_connection(connection);
 
-  LOG("heu");
-
-  return 0;
+  return EXIT_SUCCESS;
 }
